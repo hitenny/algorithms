d7415691f44b857864e0d43659bf4379c99e4b0a
hitenny <hitenny@gmail.com>
2021-11-03 19:11:13 +0530

    Create MaxSumPath.java

---
+++ BinaryTree/MaxSumPath.java
@@ -0,0 +1,23 @@
+class Solution {
+    private int maxSum = Integer.MIN_VALUE;
+    
+    public int maxPathSum(TreeNode root) {
+        calcSum(root);
+        return maxSum;
+    }
+    
+    private int calcSum(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        
+        int l = calcSum(root.left);  //return current max sum of left sub-tree
+        int r = calcSum(root.right); //return current max sum of right sub-tree
+            
+        int sum = root.val + l + r;  //sum of left, right and root
+        int curMaxSum = Math.max(root.val, root.val + Math.max(l, r));  // Take max of root or root + max of left or right sub-tree
+        maxSum = Math.max(maxSum, Math.max(curMaxSum, sum));            // Take max of all sums
+        
+        return curMaxSum;
+    }
+}
