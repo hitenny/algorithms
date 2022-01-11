9e639349e1255ff9d477b1b96c1c55681686536d
hitenny <hitenny@gmail.com>
2022-01-12 00:01:28 +0530

    Create LIS.java

---
+++ Arrays/LIS.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int result[] = new int[nums.length];
+        
+        for(int i=0; i<nums.length; i++) {
+            int maxSeq = 1;    
+            for(int j=0; j<i; j++) {
+                if(nums[i] > nums[j]) {
+                    maxSeq = Math.max(maxSeq, result[j]+1);
+                }
+            }
+            result[i] = maxSeq;
+        }
+        
+        int maxSeq = 1;
+        for(int i=0; i<nums.length; i++) {
+            maxSeq = Math.max(maxSeq, result[i]);
+        }
+        
+        return maxSeq;
+    }
+}
