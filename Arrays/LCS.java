1730663ec29eb62da88c139fb513a846c95bb0dd
hitenny <hitenny@gmail.com>
2022-01-22 00:13:18 +0530

    Update LCS.java

--- Arrays/LCS.java
+++ Arrays/LCS.java
@@ -1,6 +1,9 @@
 //LC: https://leetcode.com/problems/longest-common-subsequence
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
+        //int[][] dp = new int[text1.length()][text2.length()]; 
+        //return LCS(text1, text2, 0, 0, dp);
+        
         int[][] dp = new int[text1.length()+1][text2.length()+1];
         
         for(int i=0; i<=text1.length(); i++) {
@@ -19,6 +22,26 @@ class Solution {
         }
         
         return dp[text1.length()][text2.length()];
+    }
+    
+    // Recursive Top Down approach. DFS with memoization
+    public int LCS(String text1, String text2, int i, int j ,int[][] dp) {
+        if(i == text1.length() || j == text2.length()) {
+            return 0;
+        }
+        
+        if(dp[i][j] > 0) {
+            return dp[i][j];
+        }
+        
+        if(text1.charAt(i) == text2.charAt(j)) {
+            dp[i][j] = 1 + LCS(text1, text2, i+1, j+1, dp);
+            return dp[i][j];
+        }
+        else {
+            dp[i][j] = Math.max(LCS(text1, text2, i+1, j, dp), LCS(text1, text2, i, j+1, dp));
+            return dp[i][j];
+        }
     }
 }
 
