64923297b12f7d218bdc188e85e848bf144e21c1
hitenny <hitenny@gmail.com>
2022-03-09 09:09:06 +0530

    Update ReverseInKGroups.java

--- LinkedList/ReverseInKGroups.java
+++ LinkedList/ReverseInKGroups.java
@@ -1,3 +1,4 @@
+//Recursive
 public ListNode reverseKGroup(ListNode head, int k) {
     ListNode curr = head;
     int count = 0;
@@ -19,3 +20,24 @@ public ListNode reverseKGroup(ListNode head, int k) {
     }
     return head;
 }
+
+//Iterative
+public ListNode reverseKGroup(ListNode head, int k) {
+        int n = 0;
+        for (ListNode i = head; i != null; n++, i = i.next);
+        
+        ListNode dmy = new ListNode(0);
+        dmy.next = head;
+        for(ListNode prev = dmy, tail = head; n >= k; n -= k) {
+            for (int i = 1; i < k; i++) {
+                ListNode next = tail.next.next;
+                tail.next.next = prev.next;
+                prev.next = tail.next;
+                tail.next = next;
+            }
+            
+            prev = tail;
+            tail = tail.next;
+        }
+        return dmy.next;
+    }
