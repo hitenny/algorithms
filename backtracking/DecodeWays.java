3e77329140e35f053edb982a6af745750bd4172e
hitenny <hitenny@gmail.com>
2022-10-09 11:23:11 +0530

    Update DecodeWays.java

--- backtracking/DecodeWays.java
+++ backtracking/DecodeWays.java
@@ -1,5 +1,6 @@
 //LC: https://leetcode.com/problems/decode-ways
 class Solution {
+    //Recursion O(2^n)
     public int numDecodings(String s) {
         if(s.isEmpty()) {
             return -1;
@@ -14,6 +15,25 @@ class Solution {
         
         Map<Integer, Integer> memo = new HashMap<>();
         return decode(s, 0, memo);
+    }
+    
+    //DP O(n)
+    public int numDecodings(String s) {
+        int[] dp = new int[s.length()+1];
+        
+        dp[s.length()] = 1;
+        for(int i=s.length()-1; i>=0; i--) {
+            if(s.charAt(i) != '0') {
+                dp[i] = dp[i+1];
+            }
+            
+            if(i < s.length()-1 && (s.charAt(i) == '1' || 
+                                    (s.charAt(i) == '2' && s.charAt(i+1) < '7'))) {
+                dp[i] += dp[i+2];
+            }
+        }
+               
+        return dp[0];
     }
     
     public int decode(String s, int index, Map<Integer, Integer> memo) {
